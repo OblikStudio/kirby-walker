84c8335a8b3b0fbfa33c5d751dd20a9b1b67ef90
Hristiyan Dodov <h.dodov@gmail.com>
2019-03-13 19:36:07 +0200

    Importer WIP

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 namespace KirbyExporter;
 
 include_once 'src/Exporter.php';
+include_once 'src/Importer.php';
 include_once 'src/KirbytagParser.php';
 
 \Kirby::plugin('oblik/exporter', [
@@ -10,8 +11,20 @@ include_once 'src/KirbytagParser.php';
       [
         'pattern' => 'export',
         'action' => function () {
-          $exporter = new Exporter();
+          $exporter = new Exporter('en', ['page' => 'series']);
           return $exporter->export();
+        }
+      ],
+      [
+        'pattern' => 'import',
+        'method' => 'GET',
+        'action' => function () {
+          // $postData = file_get_contents('php://input');
+          $postData = file_get_contents(__DIR__ . DS . 'import.json');
+          $input = json_decode($postData, true);
+          
+          $importer = new Importer('bg');
+          return $importer->import($input);
         }
       ]
     ]
--- src/Importer.php
+++ src/Importer.php
@@ -1,131 +1,120 @@
 <?php
-namespace Memsource;
+namespace KirbyExporter;
 
-use Yaml;
+use yaml;
 
 class Importer {
-    private $lang = null;
+  private $language = null;
 
-    public static function clean ($data) {
-        foreach ($data as $key => $value) {
-            if (is_array($value)) {
-                $data[$key] = static::clean($value);
+  function __construct ($language) {
+    $this->language = $language;
+  }
 
-                if (count($data[$key]) === 0) {
-                    unset($data[$key]);
-                }
-            } else {
-                $hasPrintableChars = preg_match('/[^[:cntrl:]\s]/', $value);
-
-                if (empty($value) || !$hasPrintableChars) {
-                    unset($data[$key]);
-                }
-            }
-        }
-
-        return $data;
+  public static function clean ($data) {
+    foreach ($data as $key => $value) {
+      if (is_array($value)) {
+        $data[$key] = static::clean($value);
+      }
     }
 
-    public static function revertKirbytagXML ($data) {
-        foreach ($data as $key => $value) {
-            if (is_array($value)) {
-                $data[$key] = static::revertKirbytagXML($value);
-            } else if (is_string($value)) {
-                $data[$key] = KirbytagXML::revert($value);
-            }
-        }
+    return array_filter($data);
+  }
 
-        return $data;
+  public static function revertKirbytagXML ($data) {
+    foreach ($data as $key => $value) {
+      if (is_array($value)) {
+        $data[$key] = static::revertKirbytagXML($value);
+      } else if (is_string($value)) {
+        $data[$key] = KirbytagXML::revert($value);
+      }
     }
 
-    public function updatePage ($page, $data) {
-        // Clean the input data so that empty strings won't overwrite the non-
-        // empty default language values later.
-        $data = static::clean($data);
+    return $data;
+  }
 
-        if (count($data) === 0) {
-            return; // nothing to update
-        }
+  public function updatePage ($page, $data) {
+    // Clean the input data so that empty strings won't overwrite the non-
+    // empty default language values later.
+    $data = static::clean($data);
 
-        // Using defaultLang instead of $this->lang to get the original
-        // content of the language. That's because it's not guaranteed that
-        // the current translated content will be synced with the default
-        // language one, especially with structured fields (proven).
-        $currentData = $page->content($this->defaultLang->code)->data();
-        $normalizedData = array();
-
-        // Make up an array with the current data and normalize it by parsing
-        // YAML and extracting field values.
-        foreach ($currentData as $key => $field) {
-            $normalizedData[$key] = $field->value();
-
-            if (!empty($data[$key]) && is_array($data[$key])) {
-                // If the translated value is an array, that means this
-                // field was a structure when it was exported, so it must
-                // still be a structure and should be parsed.
-
-                try {
-                    $normalizedData[$key] = Yaml::read($normalizedData[$key]);
-                } catch (\Exception $e) {}
-            }
-        }
-
-        $mergedData = array_replace_recursive($normalizedData, $data);
-
-        // Encode all arrays back to YAML because that's how Kirby stores
-        // them. If they are not pased, an empty value will be saved.
-        foreach ($mergedData as $key => $value) {
-            if (is_array($value)) {
-                $mergedData[$key] = Yaml::encode($mergedData[$key]);
-            }
-        }
-
-        $page->update(static::revertKirbytagXML($mergedData), $this->lang);
+    if (empty($data)) {
+      return; // nothing to update
     }
 
-    public function importPages ($data) {
-        foreach ($data as $pageId => $value) {
-            $page = null;
+    $fieldBlueprints = $page->blueprint()->fields();
+    $defaultData = [];
 
-            if ($pageId === '$site') {
-                $page = site();
-            } else {
-                $page = site()->children()->find($pageId);
-            }
+    foreach ($fieldBlueprints as $key => $blueprint) {
+      $field = $page->$key();
 
-            if ($page) {
-                $this->updatePage($page, $value, $this->lang);
-            }
-        }
+      if ($blueprint['type'] === 'structure') {
+        $defaultData[$key] = $field->yaml();
+      } else {
+        $defaultData[$key] = $field->value();
+      }
     }
 
-    public function importVariables ($data) {
-        $dir = kirby()->roots()->languages();
+    $mergedData = array_replace_recursive($defaultData, $data);
 
-        if (!is_dir($dir)) {
-            mkdir($dir);
-        }
-
-        $file = $dir . DS . $this->lang . '.yml';
-        $encoded = Yaml::encode($data);
-
-        file_put_contents($file, $encoded);
+    // Encode all arrays back to YAML because that's how Kirby stores
+    // them. If they are not pased, an empty value will be saved.
+    foreach ($mergedData as $key => $value) {
+      if (
+        isset($fieldBlueprints[$key]) &&
+        $fieldBlueprints[$key]['type'] === 'structure'
+      ) {
+        $mergedData[$key] = yaml::encode($value);
+      }
     }
 
-    public function import ($data, $lang) {
-        $this->lang = $lang;
-        $this->defaultLang = site()->defaultLanguage();
+    // static::revertKirbytagXML($mergedData)
+    $page->update($mergedData, $this->language);
+  }
 
-        if (isset($data['content'])) {
-            $data = $data['content'];
-        }
+  public function importPages ($data) {
+    foreach ($data as $pageId => $value) {
+      $page = null;
 
-        if (isset($data['pages'])) {
-            $this->importPages($data['pages']);
-        }
+      if ($pageId === '$site') {
+        $page = site();
+      } else {
+        $page = site()->children()->find($pageId);
+      }
 
-        if (isset($data['variables'])) {
-            $this->importVariables($data['variables']);
-        }
+      if ($page) {
+        $this->updatePage($page, $value, $this->lang);
+      }
     }
+  }
+
+  public function importVariables ($data) {
+    $dir = kirby()->roots()->languages();
+
+    if (!is_dir($dir)) {
+      mkdir($dir);
+    }
+
+    $file = $dir . DS . $this->lang . '.yml';
+    $encoded = Yaml::encode($data);
+
+    file_put_contents($file, $encoded);
+  }
+
+  public function import ($data) {
+    $data = static::clean($data);
+
+    $this->defaultLang = kirby()->defaultLanguage()->code();
+
+    if (isset($data['site'])) {
+      $this->updatePage(site(), $data['site']);
+    }
+
+    // if (isset($data['pages'])) {
+    //   $this->importPages($data['pages']);
+    // }
+
+    // if (isset($data['variables'])) {
+    //   $this->importVariables($data['variables']);
+    // }
+  }
 }
