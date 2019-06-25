a096c66839527677c9865ea97b39f0a51f507e3d
Hristiyan Dodov <h.dodov@gmail.com>
2019-06-25 17:38:52 +0300

    Add multiple languages

---
+++ tests/kirby/languages/bg.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+    'code' => 'bg',
+    'default' => false,
+    'direction' => 'ltr',
+    'locale' => [
+        'bg_BG'
+    ],
+    'name' => 'Bulgarian',
+    'translations' => [
+
+    ],
+    'url' => NULL
+];
