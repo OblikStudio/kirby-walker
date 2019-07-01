0a94b79742bf0408dddedaa1e3328f26bf790325
Hristiyan Dodov <h.dodov@gmail.com>
2019-07-01 16:33:01 +0300

    Refactor exporter test fixture logic

--- tests/suites/ExporterTest.php
+++ tests/suites/ExporterTest.php
@@ -2,53 +2,48 @@
 
 use PHPUnit\Framework\TestCase;
 use KirbyOutsource\Exporter;
-
-// add test for blueprint title
-
-$exporter = new Exporter([
-  'blueprints' => [
-    'title' => [
-      'type' => 'text'
-    ]
-  ],
-  'fields' => [
-    'pages' => [
-      'exporter' => [
-        'ignore' => true
-      ]
-    ]
-  ],
-  'fieldPredicate' => function ($field, $blueprint) {
-    return $blueprint['translate'] ?? true;
-  }
-]);
-
-$GLOBALS['data'] = $exporter->export(site());
-relog($GLOBALS['data']);
+use Kirby\Cms\Pages;
 
 final class ExporterTest extends TestCase {
+  public static $data;
+
+  public static function setUpBeforeClass (): void {
+    $exporter = new Exporter([
+      'blueprints' => option('oblik.exporter.blueprints'),
+      'fields' => option('oblik.exporter.fields'),
+      'fieldPredicate' => function ($field, $blueprint) {
+        return $blueprint['translate'] ?? true;
+      }
+    ]);
+
+    $models = new Pages();
+    $models->prepend(site());
+    $exportedData = $exporter->export($models);
+    self::$data = $exportedData['site'];
+  }
+
   public function testEmptyBlueprint () {
     // Kirby should convert empty blueprints to valid ones with default values
     // on which the exporter relies.
-    $this->assertArrayHasKey('text', $GLOBALS['data']['site']);
+    $this->assertArrayHasKey('text', self::$data);
   }
 
   public function testIgnoredField () {
-    $this->assertArrayNotHasKey('ignoredfield', $GLOBALS['data']['site']);
+    $this->assertArrayNotHasKey('ignoredfield', self::$data);
   }
 
   public function testFieldPredicate () {
-    $this->assertArrayNotHasKey('nottranslated', $GLOBALS['data']['site']);
+    $this->assertArrayNotHasKey('nottranslated', self::$data);
   }
 
   public function testEmptyStructure () {
-    $this->assertArrayNotHasKey('emptystruct', $GLOBALS['data']['site']);
+    $this->assertArrayNotHasKey('emptystruct', self::$data);
   }
 
   public function testYamlWhitelist () {
     $keys = array_keys(site()->yamlField()->yaml());
     $whitelist = site()->blueprint()->fields()['yamlField']['exporter']['yaml'];
-    $data = $GLOBALS['data']['site']['yamlfield'];
+    $data = self::$data['yamlfield'];
 
     foreach ($keys as $key) {
       if (in_array($key, $whitelist)) {
@@ -61,15 +56,15 @@ final class ExporterTest extends TestCase {
 
   public function testYamlAllKeys () {
     $keys = site()->yamlFieldAll()->yaml();
-    $this->assertEquals($GLOBALS['data']['site']['yamlfieldall'], $keys);
+    $this->assertEquals(self::$data['yamlfieldall'], $keys);
   }
 
   public function testYamlInStructure () {
-    $keys = array_keys($GLOBALS['data']['site']['struct'][0]['yamlfield']);
+    $keys = array_keys(self::$data['struct'][0]['yamlfield']);
     $this->assertEquals($keys, ['text']);
   }
 
   public function testFalsyValueInStructure () {
-    $this->assertArrayHasKey('falsystructurefield', $GLOBALS['data']['site']['struct'][0]);
+    $this->assertArrayHasKey('falsystructurefield', self::$data['struct'][0]);
   }
 }
