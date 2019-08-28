011f43ec729cf82d902f7a91674ea1064885a7f4
Hristiyan Dodov <h.dodov@gmail.com>
2019-08-28 17:12:27 +0300

    Add Synchronizer

--- hooks.php
+++ hooks.php
@@ -2,32 +2,7 @@
 
 namespace Oblik\Outsource;
 
-function markModel($model, $lang)
-{
-
-    $marker = new Marker();
-    $markedData = $marker->walk($model);
-    $model = $model->update($markedData, $lang);
-    return $markedData;
-}
-
-function syncModel($model, $data, $targetLang)
-{
-    $syncer = new Syncer();
-    foreach ($model->translations() as $translation) {
-        $lang = $translation->code();
-
-        if ($lang !== $targetLang) {
-            $syncer->settings['language'] = $lang;
-
-            if ($syncedData = $syncer->walk($model, $data)) {
-                $model->update($syncedData, $lang);
-            }
-        }
-    }
-}
-
-function synchronizeModel($model)
+function sync($model)
 {
     /**
      * Would be better to use the actual updated translation of the
@@ -35,23 +10,21 @@ function synchronizeModel($model)
      * @see https://github.com/getkirby/ideas/issues/396
      */
     $updatedLang = kirby()->languageCode();
+    $synchronizer = new Synchronizer($model, $updatedLang);
 
-    // Add mark structure entries by adding specified sync IDs
-    $data = markModel($model, $updatedLang);
-
-    // After the updated translation had its entries marked with IDs,
-    // synchronize all other translations with it.
-    syncModel($model, $data, $updatedLang);
+    if ($data = $synchronizer->mark()) {
+        $synchronizer->sync($data);
+    }
 }
 
 return [
     'site.update:after' => function ($site) {
-        synchronizeModel($site);
+        sync($site);
     },
     'file.update:after' => function ($file) {
-        synchronizeModel($file);
+        sync($file);
     },
     'page.update:after' => function ($page) {
-        synchronizeModel($page);
+        sync($page);
     }
 ];
---
+++ src/Synchronizer.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Oblik\Outsource;
+
+class Synchronizer
+{
+    public $model;
+    public $lang;
+
+    public function __construct($model, $lang)
+    {
+        $this->model = $model;
+        $this->lang = $lang;
+    }
+
+    public function mark()
+    {
+        $marker = new Marker();
+
+        if ($data = $marker->walk($this->model)) {
+            $this->model = $this->model->update($data, $this->lang);
+        }
+
+        return $data;
+    }
+
+    public function sync(array $input)
+    {
+        $syncer = new Syncer();
+
+        foreach ($this->model->translations() as $translation) {
+            $lang = $translation->code();
+
+            if ($lang !== $this->lang) {
+                $syncer->settings['language'] = $lang;
+
+                if ($data = $syncer->walk($this->model, $input)) {
+                    $this->model->update($data, $lang);
+                }
+            }
+        }
+    }
+}
