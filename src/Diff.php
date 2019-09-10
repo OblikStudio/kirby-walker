624c913d25a981be5225a822c995c90687926dfd
Hristiyan Dodov <h.dodov@gmail.com>
2019-09-10 12:51:15 +0300

    Finalize Diff, add tests

--- src/Diff.php
+++ src/Diff.php
@@ -2,13 +2,17 @@
 
 namespace Oblik\Outsource;
 
+/**
+ * Recursively returns the differences between two input arrays. Attempts to
+ * compare arrays with IDs according to those IDs.
+ */
 class Diff
 {
     /**
      * Invokes a callback to replace entries in input A using any matching ones
      * in input B. Missing entries are left as-is.
      */
-    public static function processIdentified (
+    public static function processKeyedArray (
         array $inputA,
         array $inputB,
         callable $callback,
@@ -21,9 +25,11 @@ class Diff
             $entryB = $inputB[$id] ?? null;
 
             if ($entryB) {
-                $entryA = $callback($entryA, $entryB, $key);
+                $entryA = $callback($entryA, $entryB);
 
                 if (is_array($entryA)) {
+                    // Make sure the ID sticks with the entry so it still
+                    // remains identifiable.
                     $entryA[$key] = $id;
                 }
             }
@@ -32,15 +38,17 @@ class Diff
         return $inputA;
     }
 
-    public function process($data, $snapshot, $ignoreKey = null)
+    public static function process($data, $snapshot)
     {
         $result = null;
 
         if (is_array($data) && is_array($snapshot)) {
             $ids = array_column($data, 'id');
 
-            if (count($ids) > 0) {
-                $result = self::processIdentified($data, $snapshot, [$this, 'process']);
+            // If all entries have IDs, assume the array is keyed and process
+            // entries according to their IDs.
+            if (count($ids) === count($data)) {
+                $result = self::processKeyedArray($data, $snapshot, [self::class, 'process']);
                 $result = array_filter($result);
 
                 if (count($result) === 0) {
@@ -49,7 +57,7 @@ class Diff
             } else {
                 foreach ($data as $key => $entry) {
                     $snapshotEntry = $snapshot[$key] ?? null;
-                    $diff = $this->process($entry, $snapshotEntry);
+                    $diff = self::process($entry, $snapshotEntry);
 
                     if ($diff !== null) {
                         $result[$key] = $diff;
--- tests/suites/DiffTest.php
+++ tests/suites/DiffTest.php
@@ -3,17 +3,44 @@
 namespace Oblik\Outsource;
 
 use PHPUnit\Framework\TestCase;
-use Kirby\Cms\Pages;
 
 final class DiffTest extends TestCase
 {
+    public static $data;
+
     public static function setUpBeforeClass(): void
     {
         $data = json_decode(file_get_contents(realpath(__DIR__ . '/../fixtures/diff-actual.json')), true);
         $snapshot = json_decode(file_get_contents(realpath(__DIR__ . '/../fixtures/diff-snapshot.json')), true);
 
-        $diff = new Diff();
-        $result = $diff->process($data, $snapshot);
-        var_export($result);
+        self::$data = Diff::process($data, $snapshot);
+    }
+
+    public function testSameContentRemoved()
+    {
+        $this->assertArrayNotHasKey('text', self::$data);
+    }
+
+    public function testArrayHasOnlyChangedValue()
+    {
+        $this->assertArrayHasKey('text', self::$data['yamlfield']);
+        $this->assertArrayNotHasKey('id', self::$data['yamlfield']);
+    }
+
+    public function testArrayIndexPreserved()
+    {
+        $this->assertArrayHasKey(1, self::$data['struct']);
+        $this->assertArrayNotHasKey(0, self::$data['struct']);
+    }
+
+    public function testKeyedArray()
+    {
+        $ids = array_column(self::$data['synced'], 'id');
+        $this->assertEquals(['b'], $ids);
+    }
+
+    public function testEntryIdPreserved()
+    {
+        $this->assertEquals('b-b', self::$data['synced'][1]['nested'][2]['id']);
     }
 }
