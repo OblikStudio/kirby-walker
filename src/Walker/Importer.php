a5cdc2b3379924a7f74f1fa7c11253d831aa2bfc
Hristiyan Dodov <h.dodov@gmail.com>
2021-10-25 17:51:58 +0300

    feat: blocks import logic

--- src/Walker/Importer.php
+++ src/Walker/Importer.php
@@ -2,8 +2,10 @@
 
 namespace Oblik\Walker\Walker;
 
+use Error;
 use Kirby\Cms\Field;
 use Kirby\Cms\ModelWithContent;
+use Kirby\Form\Field as FormField;
 
 class Importer extends Walker
 {
@@ -28,5 +30,34 @@ class Importer extends Walker
 	protected function walkFieldDefault($field, $settings, $input)
 	{
 		return !empty($input) ? $input : $field->value();
+	}
+
+	protected function walkFieldBlocks($field, $settings, $input)
+	{
+		$data = [];
+
+		if (is_array($input)) {
+			$input = array_column($input, null, 'id');
+		}
+
+		$blocks = $field->toBlocks();
+		$sets = FormField::factory('blocks', $settings)->fieldsets();
+
+		foreach ($blocks as $id => $block) {
+			$set = $sets->get($block->type());
+
+			if (empty($set)) {
+				throw new Error('Missing fieldset for block type: "' . $block->type() . '"');
+			}
+
+			$childData = $block->toArray();
+			$childData['content'] = $this->walkContent($block->content(), $set->fields(), $input[$id]['content'] ?? null);
+
+			if (!empty($childData['content'])) {
+				$data[] = $childData;
+			}
+		}
+
+		return $data;
 	}
 }
--- tests/Walker/ImporterTest.php
+++ tests/Walker/ImporterTest.php
@@ -4,6 +4,7 @@ namespace Oblik\Walker\Walker;
 
 use Kirby\Cms\App;
 use Kirby\Cms\Page;
+use Kirby\Data\Json;
 use Oblik\Walker\TestCase;
 
 final class ImporterTest extends TestCase
@@ -85,5 +86,105 @@ final class ImporterTest extends TestCase
 		$result = (new Importer())->walk($page, null, $content);
 
 		$this->assertEquals($original, $result);
+	}
+
+	public function testImportsBlocks()
+	{
+		$page = new Page([
+			'slug' => 'test',
+			'content' => [
+				'text' => Json::encode([
+					[
+						'content' => [
+							'text' => '<p>original 1</p>'
+						],
+						"id" => "c14c1bff-6a9d-4179-9aa9-b9df25dca891",
+						"isHidden" => false,
+						"type" => "text"
+					],
+					[
+						'content' => [
+							'text' => '<p>original 2</p>'
+						],
+						"id" => "9da43326-6118-4f4a-b4cd-23ca5bd5c35f",
+						"isHidden" => false,
+						"type" => "text"
+					],
+					[
+						'content' => [
+							'text' => '<p>original 3</p>'
+						],
+						"id" => "fdd25d51-8ac5-42aa-8476-b63dac8c577b",
+						"isHidden" => false,
+						"type" => "text"
+					]
+				])
+			],
+			'blueprint' => [
+				'fields' => [
+					'text' => [
+						'type' => 'blocks',
+						'fieldsets' => [
+							'text'
+						]
+					]
+				]
+			]
+		]);
+
+		$import = [
+			'text' => [
+				[
+					'content' => [
+						'text' => '<p>imported 3</p>'
+					],
+					"id" => "fdd25d51-8ac5-42aa-8476-b63dac8c577b"
+				],
+				[
+					'content' => [
+						'text' => '<p>outdated block</p>'
+					],
+					"id" => "bb8ca9fd-d1ee-4567-a130-4946afdee9a8"
+				],
+				[
+					'content' => [
+						'text' => '<p>imported 1</p>'
+					],
+					"id" => "c14c1bff-6a9d-4179-9aa9-b9df25dca891"
+				]
+			]
+		];
+
+		$expected = [
+			'text' => [
+				[
+					'content' => [
+						'text' => '<p>imported 1</p>'
+					],
+					"id" => "c14c1bff-6a9d-4179-9aa9-b9df25dca891",
+					"isHidden" => false,
+					"type" => "text"
+				],
+				[
+					'content' => [
+						'text' => '<p>original 2</p>'
+					],
+					"id" => "9da43326-6118-4f4a-b4cd-23ca5bd5c35f",
+					"isHidden" => false,
+					"type" => "text"
+				],
+				[
+					'content' => [
+						'text' => '<p>imported 3</p>'
+					],
+					"id" => "fdd25d51-8ac5-42aa-8476-b63dac8c577b",
+					"isHidden" => false,
+					"type" => "text"
+				]
+			]
+		];
+
+		$result = (new Importer())->walk($page, null, $import);
+		$this->assertEquals($expected, $result);
 	}
 }
