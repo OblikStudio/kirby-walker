8f2f5a90d65b49db101145f0260df485ac15ba87
Hristiyan Dodov <h.dodov@gmail.com>
2021-12-03 16:09:12 +0200

    ref: use kirby's load() function to load classes
    If Composer's autoload feature is used, you get errors that the class is not loaded
    when you symlink the plugin in another project. This fixes that.

--- index.php
+++ index.php
@@ -4,4 +4,13 @@ namespace Oblik\Walker;
 
 use Kirby\Cms\App;
 
+load([
+	'Oblik\\Walker\\Serialize\\KirbyTags' => 'Serialize/KirbyTags.php',
+	'Oblik\\Walker\\Serialize\\Markdown' => 'Serialize/Markdown.php',
+	'Oblik\\Walker\\Serialize\\Template' => 'Serialize/Template.php',
+	'Oblik\\Walker\\Walker\\Exporter' => 'Walker/Exporter.php',
+	'Oblik\\Walker\\Walker\\Importer' => 'Walker/Importer.php',
+	'Oblik\\Walker\\Walker\\Walker' => 'Walker/Walker.php'
+], __DIR__ . '/src');
+
 App::plugin('oblik/walker', []);
